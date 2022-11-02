d71f71e2af7b1cb89104eb7d534822871a9108f0
Daniel Donnelly <dcdonnelly@hotmail.co.uk>
2022-11-02 13:11:25 +0000

    Updates

---
+++ precache-manifest.d0419f6ce8452e5733a7a8b5b2ce5a1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6178fe6758cb43a58ef0",
+    "url": "/robofriends/static/css/main.b5d4696a.chunk.css"
+  },
+  {
+    "revision": "6178fe6758cb43a58ef0",
+    "url": "/robofriends/static/js/main.6178fe67.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robofriends/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "261dae3e4e1bb500ca45",
+    "url": "/robofriends/static/css/2.bf9bac47.chunk.css"
+  },
+  {
+    "revision": "261dae3e4e1bb500ca45",
+    "url": "/robofriends/static/js/2.261dae3e.chunk.js"
+  },
+  {
+    "revision": "6bf665704170461feaf8",
+    "url": "/robofriends/static/js/runtime~main.6bf66570.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "14979bd18755332e2fae2c7ca4c3c50f",
+    "url": "/robofriends/index.html"
+  }
+];
